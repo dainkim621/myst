ccdd3f7fbcff41e6b7d213e0f607ec7f88761a2e
Dain Kim <126754209+dainkim621@users.noreply.github.com>
2023-05-16 11:32:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,5 @@ st.sidebar.radio("Pick your gender", ["Male", "Female"])
 container = st.container()
 container.write("This is written inside the container")
 st.write("This is written outside the container")
+
+# bar = Barchart
